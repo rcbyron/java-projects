31cd1c8af6b499ecb53844df41689313ccbc1f23
Robert Byron <rcbyron@utexas.edu>
2016-06-16 14:51:52 -0500

    Added assignment 2

---
+++ mastermind-game/src/assignment2/GameConfiguration.java
@@ -0,0 +1,17 @@
+/*  * EE422C Project 2 (Mastermind) submission by
+ * Robert (Connor) Byron
+ * rcb2746
+ * <Student 2 Name>
+ * <Student 2 EID>
+ * https://github.com/rcbyron/java/mastermind-game
+ * Slip days used: 0
+ * Summer 2016
+ */
+
+package assignment2;
+
+public class GameConfiguration {
+	public static final int guessNumber = 12;
+	public static final String[] colors = {"B","G","O","P","R","Y"};
+	public static final int pegNumber = 4;
+}
