00b8e80d0244cd7c9ba63b1472c85c14e230f368
Joel Guo <jguo48@gmail.com>
2016-07-21 16:06:55 -0500

    made two new critters named after ECE Professors Banerjee and Patt

---
+++ critter-world/src/project4/Sanjay.java
@@ -0,0 +1,17 @@
+package project4;
+
+public class Sanjay extends Critter {
+
+	@Override
+	public void doTimeStep() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean fight(String oponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ critter-world/src/project4/Yale.java
@@ -0,0 +1,17 @@
+package project4;
+
+public class Yale extends Critter {
+
+	@Override
+	public void doTimeStep() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean fight(String oponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
