9b788dcef28d3647340083d2d9fff8b665b05205
Joel Guo <jguo48@gmail.com>
2016-07-22 16:32:20 -0500

    Valvano and Nandakumar critters

---
+++ critter-world/src/project4/Jon.java
@@ -0,0 +1,20 @@
+package project4;
+
+public class Jon extends Critter {
+
+	@Override
+	public String toString() { return "J"; }
+	
+	@Override
+	public void doTimeStep() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean fight(String oponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ critter-world/src/project4/Vallath.java
@@ -0,0 +1,19 @@
+package project4;
+
+public class Vallath extends Critter {
+
+	@Override
+	public String toString() { return "V";	}
+	
+	@Override
+	public void doTimeStep() {
+		
+	}
+
+	@Override
+	public boolean fight(String oponent) {
+		
+		return false;
+	}
+
+}
